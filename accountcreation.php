16415b2337561f2e0b9a6740b4817bb4d52e75bb
Exitare <business.exitare@icloud.com>
2017-04-04 22:01:18 +0200

    Add new stuff

--- accountcreation.php
+++ accountcreation.php
@@ -1,17 +1,45 @@
 <?php
-$servername = "localhost";
-$db_username = "";
-$db_password = "";
-$db_name = "wotlkauth";
-
-
 
 if (array_key_exists("username", $_POST) && array_key_exists("password", $_POST) && array_key_exists("betakey",$_POST)) {
-	createAccount($_POST["username"], base64_encode($_POST["password"]),$_POST["betakey"]);
+	createAccount($_POST["username"], ($_POST["password"]),$_POST["betakey"]);
 }
-        
+ 
+else {
+	echo "Missing something";
+}
 
 function createAccount($username,$password,$betakey){
+	
+	$servername = "127.0.0.1";
+	$db_username = "Exitare";
+	$db_password = "raphaelk1";
+	$db_name = "wotlkauth";
+	
+	$up_username = strtoupper($username);
+	$up_password = strtoupper($password);
+	$isBetaKeyused = checkbetakey($betakey);
+	
+	if($betakey == ""){
+		echo "Without BetaKey no Accountcreation!";
+		return;
+	}
+	
+	if($isBetaKeyused == "1"){
+		echo "BetaKey is used";
+		return;
+	}
+	
+	if($isBetaKeyused == "-1"){
+		echo "BetaKey not found!";
+		return;
+	}
+	
+	$accountExist = checkifAccountexist($up_username);
+	if($accountExist == "1"){
+		echo "Account exists";
+		return;
+	}
+	
 	// Create connection
 	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
 	// Check connection
@@ -20,43 +48,118 @@ function createAccount($username,$password,$betakey){
 		echo "Connection failed";
 	}
 	
-	$hashed_pw = sha1($username .":". $password);
+	$passwordbuilder = $up_username .":".$up_password;
+	$hashed_pw = sha1($passwordbuilder);
+	
+	//echo $hashed_pw;
+	$final_pw = strtoupper ($hashed_pw);
 
 	// prepare and bind
 	$stmt = $conn->prepare("INSERT INTO account (username, sha_pass_hash) VALUES (?,?)");
-	$stmt->bind_param("sss", $user_name, $user_password);
+	$stmt->bind_param("ss",$user_name, $user_password);
 
 	// set parameters and execute
-	$user_name = $username;
-	$user_password = $hashed_pw;
+	$user_name = $up_username;
+	$user_password = $final_pw;
 	$stmt->execute();
 
+	//betaKeyUsed($betakey);
 
 	$stmt->close();
 	$conn->close();
 	return;
 }
 
+function betaKeyUsed($betakey){
+	$servername = "127.0.0.1";
+	$db_username = "Exitare";
+	$db_password = "raphaelk1";
+	$db_name = "wotlkauth";
+	
+	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+		echo "Connection failed";
+	}
+	
+
+	$stmt = $conn->prepare("Update betakey set `active` = 1 where `betakeynr` = ?");
+	$stmt->bind_param("s",$db_betakeynr);
+
+	// set parameters and execute
+	$db_betakeynr = $betakey;
+	$stmt->execute();
 
 
-function getBetakey($betakey){
-	$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	$stmt->close();
+	$conn->close();
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+function checkifAccountexist($username){
+	$servername = "127.0.0.1";
+	$db_username = "Exitare";
+	$db_password = "raphaelk1";
+	$db_name = "wotlkauth";
+	
 
-if ($conn->query($sql) === TRUE) {
-    $last_id = $conn->insert_id;
-    echo "New record created successfully. Last inserted ID is: " . $last_id;
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+	
+	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+	
+	$sql = "SELECT id, username FROM account";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+		if($row["username"] == $username){
+			return "1";
+		}
+		
+	
+      
+    }
+	return "0";
+	
+	$conn->close();
+	
+	}
 }
 
-$conn->close();
+function checkbetakey($betakey){
+	$servername = "127.0.0.1";
+	$db_username = "Exitare";
+	$db_password = "raphaelk1";
+	$db_name = "wotlkauth";
+	
+	$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "SELECT id, betakeynr, active FROM betakey";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+    // output data of each row
+		while($row = $result->fetch_assoc()) {		
+			if($row["betakeynr"] == $betakey){
+				$conn->close();
+				return $row["active"];
+			}
+			
+		}
+	} else {
+		$conn->close();
+		return "-1";
+	
+	}	
+	$conn->close();
 }
 
 ?>
--- index.php
+++ index.php
@@ -17,16 +17,15 @@ include_once("./accountcreation.php");
 <body>
 <h1>Welcome at the Homepage of  Purity Gaming!</h1>
 <p>Our Homepage is under Construction.</p>
- <form action="../index.html" method="post">
+ <form action="../index.php" method="post">
   Accountname:<br>
-  <input type="text" name="accountname"><br>
+  <input type="text" name="username" value="Test"><br>
   Accountpassword:<br>
-  <input type="password" name="password"><br>
+  <input type="password" name="password" value="test"><br>
   BetaKey: <br>
   <input type="text" name="betakey"><br>
-<button type="submit">Create Account!</button>  </p>
+<button type="submit">Create Account!</button>
 </form>
-
 
 <p><em>Thank you for your interest on  playing at our Server.</em></p>
 </body>
